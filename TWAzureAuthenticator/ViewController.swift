0f74e1a016f79d252e5bd6ecd03e7a3318f60d93
Levi <levi@azuredigitaltech.com.tw>
2019-11-22 13:37:46 +0800

    Initial Commit

---
+++ TWAzureAuthenticator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TWAzureAuthenticator
+//
+//  Created by 誠帷數位科技 on 2019/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
