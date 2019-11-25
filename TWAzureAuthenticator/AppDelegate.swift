2b1d1aad414fe8a5cfe2c08c0751acd2154985f2
Levi <levi@azuredigitaltech.com.tw>
2019-11-25 10:45:22 +0800

    creat: gitignore, pod init

--- TWAzureAuthenticator/AppDelegate.swift
+++ TWAzureAuthenticator/AppDelegate.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AuthenticatorAppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        
         return true
     }
 
