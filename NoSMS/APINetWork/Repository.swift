1c7e5ed4bc5d7e2b5a1f4d89eb8ad80fba201f15
ricky <ricky@azuredigitaltech.com.tw>
2020-04-07 13:59:52 +0800

    修改API及WebView切換URL功能，增加傳送API參數

--- NoSMS/APINetWork/Repository.swift
+++ NoSMS/APINetWork/Repository.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import UIKit
+
 enum RESTAPIURL:String {
     case VERSION =  "version"
     
@@ -37,77 +39,54 @@ final class Repository {
     private let apiClient: APIClient = APIClient()
     static let sharedInstance = Repository()
     private var currentWebAPI = ""
-    private var currentWebViewDomain = ""
-    private let defaultAPI = "https://maapi-dev.azuredigitaltech.com.tw:18443/api/"
     let defaultWebAPI = "https://maapi-dev.azuredigitaltech.com.tw:18443/api/"
     private init() {
-        currentWebAPI = getDefaultwebAPI()
-    }
-    func getCurrentAPI()-> String {
-        return currentWebAPI
+        currentWebAPI = getwebAPI()
     }
 //    init(apiClient: APIClient) {
 //        self.apiClient = apiClient
 //    }
-    private func getDefaultwebAPI()->String{
+    private var webViewURL = "https://mustauth.com/"
+    func getWebViewURL()->String {
+        return webViewURL
+    }
+    private func getwebAPI()->String{
         if let webAPI = Bundle.main.object(forInfoDictionaryKey: "webAPI") as? String {
             return webAPI
-        }else{
-            return defaultAPI
-        }
-    }
-    private func getWebAPI(domain:String)->String {
-        if let firstAPIDomain = Bundle.main.object(forInfoDictionaryKey: "defaultFirstAPIDomain") as? String,let apiDomainProtocol = Bundle.main.object(forInfoDictionaryKey: "apiDomainProtocol") as? String,let APIDomainPort = Bundle.main.object(forInfoDictionaryKey: "APIDomainPort") as? String {
-            if !APIDomainPort.isEmpty {
-                return "\(apiDomainProtocol)://\(firstAPIDomain).\(domain):\(APIDomainPort)/api/"
-            } else {
-                return "\(apiDomainProtocol)://\(firstAPIDomain).\(domain)/api/"
-            }
-            
-        }else{
-            return "https://maapi-dev.azuredigitaltech.com.tw:18443/api/"
-        }
-    }
-    private func getWebViewDomain(domain:String)->String {
-        if let firstWebViewDomain = Bundle.main.object(forInfoDictionaryKey: "defaultFirstWebViewDomain") as? String,let webViewDomainProtocol = Bundle.main.object(forInfoDictionaryKey: "webViewDomainProtocol") as? String,let webViewDomainPort = Bundle.main.object(forInfoDictionaryKey: "WebViewDomainPort") as? String {
-            if !webViewDomainPort.isEmpty {
-                return "\(webViewDomainProtocol)://\(firstWebViewDomain).\(domain):\(webViewDomainPort)/"
-            } else {
-                return "\(webViewDomainProtocol)://\(firstWebViewDomain).\(domain)/"
-            }
-            
         }else{
             return defaultWebAPI
         }
     }
-   
+    func getCurrentAPI()->String {
+        return currentWebAPI
+    }
     func postVersion(_ completion: @escaping ((Result<Version>) -> Void)){
         if let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String {
 //            print("Build Version:\(build)")
-            let parameters:[String : Any] = ["platform":"ios","version":String(describing:build)]
+//            print("uuid:\(UIDevice.current.identifierForVendor?.uuidString),model:\(UIDevice.current.modelName.lowercased().replacingOccurrences(of: " ", with: "")),os_version:\(UIDevice.current.systemVersion)")
+            let parameters:[String:Any] = ["platform":"ios","version":build,"mid":UIDevice.current.identifierForVendor?.uuidString ?? "","brand":"apple","model":UIDevice.current.modelName.lowercased().replacingOccurrences(of: " ", with: ""),"os_version":UIDevice.current.systemVersion]
             let postURL = RESTAPIURL.VERSION.url!
             let resource = PostResource(url: postURL,parameters: parameters)
             apiClient.post(resource) {[weak self] (result) in
+                guard let self = self else{return}
                 switch result {
                 case .success(let data):
                     do {
                         let str = String(decoding: data, as: UTF8.self)
                         print("jsonData:\(str)")
                         var items = try JSONDecoder().decode(Version.self, from: data)
-                        self?.version = items
-                        if items.domain.count > 0 && !items.domain[0].isEmpty{
-//                            print("compose webapi:\(self?.getWebAPI(domain: items.domain[0]))")
-                            self?.currentWebAPI = self?.getWebAPI(domain: items.domain[0]) ?? self!.getDefaultwebAPI()
-                            self?.currentWebViewDomain = self?.getWebViewDomain(domain: items.domain[0]) ?? ""
-                        }
-
+                        Repository.sharedInstance.version = items
                         items.isNeedUpdate = items.code == 1
-                        self?.version?.isNeedUpdate = items.isNeedUpdate
                         Repository.sharedInstance.version?.isNeedUpdate = items.isNeedUpdate
-//                        if items.domain.count > 0 && !items.domain[0].isEmpty{
-//
-//                            self?.currentWebAPI = "https://api.\(items.domain[0])/api/"
-//                        }
+                        if items.domain.count > 0 && !items.domain[0].isEmpty{
+
+                            self.currentWebAPI = "https://api.\(items.domain[0])/api/"
+                            if items.domain.count > 1 {
+                                self.webViewURL = "https://\(items.domain[1].replacingOccurrences(of: "“", with: "").replacingOccurrences(of: "”", with: ""))/"
+                            }else{
+                                self.webViewURL = "https://\(items.domain[0].replacingOccurrences(of: "“", with: "").replacingOccurrences(of: "”", with: ""))/"
+                            }
+                        }
                         completion(.success(items))
                     } catch {
                         completion(.failure(error))
@@ -117,5 +96,18 @@ final class Repository {
                 }
             }
         }
+    }
+}
+//https://stackoverflow.com/questions/11197509/how-to-get-device-make-and-model-on-ios
+extension UIDevice {
+    var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
     }
 }
